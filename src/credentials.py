d06e1d6d691e7c4e5618fe99e2a59395f5a9e61a
MubaraqSani <mubaraq.sani@e4email.net>
2022-08-17 09:04:57 +0100

    Added a src folder to the repo

---
+++ src/credentials.py
@@ -0,0 +1,18 @@
+from apiclient import discovery
+from httplib2 import Http
+from oauth2client import client, file, tools
+
+
+# define path variables
+credentials_file_path = '/home/mubarak/e4e-august/mlops/credentials/credentials.json'
+clientsecret_file_path = '/home/mubarak/e4e-august/mlops/credentials/client_secret.json'
+
+# define API scope
+SCOPE = 'https://www.googleapis.com/auth/drive'
+
+# define store
+store = file.Storage(credentials_file_path)
+credentials = store.get()# get access token
+if not credentials or credentials.invalid:
+    flow = client.flow_from_clientsecrets(clientsecret_file_path, SCOPE)
+    credentials = tools.run_flow(flow, store)
